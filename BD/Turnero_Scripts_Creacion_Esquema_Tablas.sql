7d4db374f69e01a6e54ff02f8bdbc4365a7a2660
Maia Majzum <169091528+maiamajzum@users.noreply.github.com>
2024-06-10 18:32:17 -0300

    Update Turnero_Scripts_Creacion_Esquema_Tablas.sql

--- BD/Turnero_Scripts_Creacion_Esquema_Tablas.sql
+++ BD/Turnero_Scripts_Creacion_Esquema_Tablas.sql
@@ -15,123 +15,88 @@ CREATE SCHEMA IF NOT EXISTS `Turnero` DEFAULT CHARACTER SET utf8mb4;
 USE `Turnero` ;
 
 -- -----------------------------------------------------
--- Table `Turnero`.`Paciente`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Turnero`.`Paciente` (
-  `id_paciente` INT NOT NULL AUTO_INCREMENT,
-  `Nombre` VARCHAR(45) NOT NULL,
-  `Apellido` VARCHAR(45) NOT NULL,
-  `DNI` INT NOT NULL,
-  PRIMARY KEY (`id_paciente`),
-  UNIQUE INDEX `id_paciente_UNIQUE` (`id_paciente` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `Turnero`.`Departamento`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Turnero`.`Departamento` (
-  `id_departamento` INT NOT NULL AUTO_INCREMENT,
-  `Nombre` VARCHAR(45) NULL,
-  PRIMARY KEY (`id_departamento`))
-ENGINE = InnoDB;
-
+CREATE TABLE `Departamento` (
+  `id_departamento` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_departamento`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- -----------------------------------------------------
 -- Table `Turnero`.`Especialidad`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Turnero`.`Especialidad` (
-  `id_especialidad` INT NOT NULL AUTO_INCREMENT,
-  `Nombre` VARCHAR(45) NULL,
-  `Departamento_id_departamento` INT NOT NULL,
-  PRIMARY KEY (`id_especialidad`, `Departamento_id_departamento`),
-  INDEX `fk_Especialidad_Departamento1_idx` (`Departamento_id_departamento` ASC) VISIBLE,
-  CONSTRAINT `fk_Especialidad_Departamento1`
-    FOREIGN KEY (`Departamento_id_departamento`)
-    REFERENCES `Turnero`.`Departamento` (`id_departamento`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Turnero`.`Turno`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Turnero`.`Turno` (
-  `id_turno` INT NOT NULL AUTO_INCREMENT,
-  `fecha` DATE NOT NULL,
-  `hora` INT NOT NULL,
-  `Paciente_id_paciente` INT NOT NULL,
-  `Especialidad_id_especialidad` INT NOT NULL,
-  PRIMARY KEY (`id_turno`, `Paciente_id_paciente`, `Especialidad_id_especialidad`),
-  INDEX `fk_Turno_Paciente1_idx` (`Paciente_id_paciente` ASC) VISIBLE,
-  INDEX `fk_Turno_Especialidad1_idx` (`Especialidad_id_especialidad` ASC) VISIBLE,
-  CONSTRAINT `fk_Turno_Paciente1`
-    FOREIGN KEY (`Paciente_id_paciente`)
-    REFERENCES `Turnero`.`Paciente` (`id_paciente`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Turno_Especialidad1`
-    FOREIGN KEY (`Especialidad_id_especialidad`)
-    REFERENCES `Turnero`.`Especialidad` (`id_especialidad`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+CREATE TABLE `Especialidad` (
+  `id_especialidad` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) DEFAULT NULL,
+  `Departamento_id_departamento` int(11) NOT NULL,
+  PRIMARY KEY (`id_especialidad`,`Departamento_id_departamento`),
+  KEY `fk_Especialidad_Departamento1_idx` (`Departamento_id_departamento`),
+  CONSTRAINT `fk_Especialidad_Departamento1` FOREIGN KEY (`Departamento_id_departamento`) REFERENCES `Departamento` (`id_departamento`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- -----------------------------------------------------
 -- Table `Turnero`.`Horario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Turnero`.`Horario` (
-  `id_Horario` INT NOT NULL AUTO_INCREMENT,
-  `hora_inicio` INT NOT NULL,
-  `hora_fin` INT NOT NULL,
-  PRIMARY KEY (`id_Horario`))
-ENGINE = InnoDB;
-
+CREATE TABLE `Horario` (
+  `id_Horario` int(11) NOT NULL AUTO_INCREMENT,
+  `hora_inicio` int(11) NOT NULL,
+  `hora_fin` int(11) NOT NULL,
+  PRIMARY KEY (`id_Horario`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- -----------------------------------------------------
 -- Table `Turnero`.`Medico`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Turnero`.`Medico` (
-  `id_medico` INT NOT NULL AUTO_INCREMENT,
-  `Nombre` VARCHAR(45) NULL,
-  `Apellido` VARCHAR(45) NULL,
-  `Horario_id_Horario` INT NOT NULL,
-  PRIMARY KEY (`id_medico`, `Horario_id_Horario`),
-  INDEX `fk_Medico_Horario1_idx` (`Horario_id_Horario` ASC) VISIBLE,
-  CONSTRAINT `fk_Medico_Horario1`
-    FOREIGN KEY (`Horario_id_Horario`)
-    REFERENCES `Turnero`.`Horario` (`id_Horario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+CREATE TABLE `Medico` (
+  `id_medico` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) DEFAULT NULL,
+  `Apellido` varchar(45) DEFAULT NULL,
+  `Horario_id_Horario` int(11) NOT NULL,
+  PRIMARY KEY (`id_medico`,`Horario_id_Horario`),
+  KEY `fk_Medico_Horario1_idx` (`Horario_id_Horario`),
+  CONSTRAINT `fk_Medico_Horario1` FOREIGN KEY (`Horario_id_Horario`) REFERENCES `Horario` (`id_Horario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- -----------------------------------------------------
 -- Table `Turnero`.`Medico_has_Especialidad`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Turnero`.`Medico_has_Especialidad` (
-  `Medico_id_medico` INT NOT NULL,
-  `Especialidad_id_especialidad` INT NOT NULL,
-  PRIMARY KEY (`Medico_id_medico`, `Especialidad_id_especialidad`),
-  INDEX `fk_Medico_has_Especialidad_Especialidad1_idx` (`Especialidad_id_especialidad` ASC) VISIBLE,
-  INDEX `fk_Medico_has_Especialidad_Medico1_idx` (`Medico_id_medico` ASC) VISIBLE,
-  CONSTRAINT `fk_Medico_has_Especialidad_Medico1`
-    FOREIGN KEY (`Medico_id_medico`)
-    REFERENCES `Turnero`.`Medico` (`id_medico`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Medico_has_Especialidad_Especialidad1`
-    FOREIGN KEY (`Especialidad_id_especialidad`)
-    REFERENCES `Turnero`.`Especialidad` (`id_especialidad`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE `Medico_has_Especialidad` (
+  `Medico_id_medico` int(11) NOT NULL,
+  `Especialidad_id_especialidad` int(11) NOT NULL,
+  PRIMARY KEY (`Medico_id_medico`,`Especialidad_id_especialidad`),
+  KEY `fk_Medico_has_Especialidad_Especialidad1_idx` (`Especialidad_id_especialidad`),
+  KEY `fk_Medico_has_Especialidad_Medico1_idx` (`Medico_id_medico`),
+  CONSTRAINT `fk_Medico_has_Especialidad_Especialidad1` FOREIGN KEY (`Especialidad_id_especialidad`) REFERENCES `Especialidad` (`id_especialidad`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Medico_has_Especialidad_Medico1` FOREIGN KEY (`Medico_id_medico`) REFERENCES `Medico` (`id_medico`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- -----------------------------------------------------
+-- Table `Turnero`.`Paciente`
+-- -----------------------------------------------------
+CREATE TABLE `Paciente` (
+  `id_paciente` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) NOT NULL,
+  `Apellido` varchar(45) NOT NULL,
+  `DNI` int(11) NOT NULL,
+  PRIMARY KEY (`id_paciente`),
+  UNIQUE KEY `id_paciente_UNIQUE` (`id_paciente`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- -----------------------------------------------------
+-- Table `Turnero`.`Turno`
+-- -----------------------------------------------------
+CREATE TABLE `Turno` (
+  `id_turno` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `Paciente_id_paciente` int(11) NOT NULL,
+  `Especialidad_id_especialidad` int(11) NOT NULL,
+  PRIMARY KEY (`id_turno`),
+  KEY `fk_Turno_Paciente1_idx` (`Paciente_id_paciente`),
+  KEY `fk_Turno_Especialidad1_idx` (`Especialidad_id_especialidad`),
+  CONSTRAINT `fk_Turno_Especialidad1` FOREIGN KEY (`Especialidad_id_especialidad`) REFERENCES `Especialidad` (`id_especialidad`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Turno_Paciente1` FOREIGN KEY (`Paciente_id_paciente`) REFERENCES `Paciente` (`id_paciente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
