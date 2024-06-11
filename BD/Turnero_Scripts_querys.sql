b7d54ae671818922eb2ceb8aa22fd34a1db09037
Maia Majzum <169091528+maiamajzum@users.noreply.github.com>
2024-06-11 12:54:52 -0300

    Update Turnero_Scripts_querys.sql

--- BD/Turnero_Scripts_querys.sql
+++ BD/Turnero_Scripts_querys.sql
@@ -4,7 +4,7 @@ SELECT id_paciente, DNI FROM paciente; -- Consulta de una sola tabla (mostrando 
 
 SELECT * FROM medico WHERE Horario_id_Horario = 1; -- Consulta de una sola tabla con WHERE
 
-SELECT * FROM turno WHERE Especialidad_id_especialidad = 1 AND hora BETWEEN 10 AND 14; -- Consulta de una sola tabla con WHERE utilizando BETWEEN
+SELECT * FROM turno WHERE Especialidad_id_especialidad = 3 AND hora BETWEEN 10 AND 14; -- Consulta de una sola tabla con WHERE utilizando BETWEEN
 
 SELECT * -- Consulta de una sola tabla con WHERE utilizando LIMIT
 FROM medico
@@ -18,13 +18,6 @@ INNER JOIN turno ON paciente.id_paciente = turno.Paciente_id_paciente
 INNER JOIN especialidad ON turno.Especialidad_id_especialidad = especialidad.id_especialidad
 WHERE especialidad.Nombre = 'Clínica Médica';
 
-
-SELECT paciente.Nombre -- Consulta de mas de una tabla con INNER JOIN y filtros
-FROM paciente
-INNER JOIN turno ON paciente.id_paciente = turno.Paciente_id_paciente
-INNER JOIN especialidad ON turno.Especialidad_id_especialidad = especialidad.id_especialidad
-INNER JOIN horario ON turno.hora = horario.id_Horario
-WHERE especialidad.Nombre = 'Clínica Médica' AND horario.hora_inicio = 9;
 
 SELECT tt.id_turno, tt.fecha, tt.hora, tp.Apellido, tp.Nombre, tp.DNI, te.Nombre -- Consulta de mas de una tabla con INNER JOIN y filtros
 FROM Turnero.Turno tt
