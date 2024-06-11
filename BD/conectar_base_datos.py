aedbc87c4424ad917808323d4bd4153c662ea35e
Maia Majzum <169091528+maiamajzum@users.noreply.github.com>
2024-06-11 15:39:10 -0300

    Update and rename conectar_base_datos.py to conectar_base_datos.py

--- BD/conectar_base_datos.py
+++ BD/conectar_base_datos.py
@@ -5,6 +5,6 @@ def conectar_base_datos():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="1sami2astorga",
+        password="clavedeusuario",
         database="Turnero"
     )
