b40db5e53d6fd6cc79eccf6b349f6d0ead13c02c
maiamajzum <maiamajzum@gmail.com>
2024-06-12 14:07:34 -0300

    conexion base de datos

---
+++ APLICACION/conectar_base_datos.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+# modificar los datos entre comillas por los correspondientes a su usuario de MYSQL
+def conectar_base_datos():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="1234",
+        database="Turnero"
+    )
